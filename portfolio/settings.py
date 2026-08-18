d67f994eb1360d587c75c44b1199d41d1cf1da99
Ruchitha <ruchithal979@gmail.com>
2026-08-18 15:00:33 +0530

    Prepare portfolio for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'main' / 'static',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
